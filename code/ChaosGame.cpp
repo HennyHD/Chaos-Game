24e9097e052b45612bc4c3aa16fcf567959bf0c6
HennyHD <166639866+HennyHD@users.noreply.github.com>
2024-04-14 22:48:11 -0700

    Update ChaosGame.cpp
    midpoint update

--- code/ChaosGame.cpp
+++ code/ChaosGame.cpp
@@ -65,10 +65,22 @@ int main()
 
         if(points.size() > 0)
         {
+		double count = 1;
 		///generate more point(s)
+		points.push_back(0,0);  //create a new point
             ///select random vertex
+		double randomVertex = rand() % 3;  //Random Vertex Tracker
             ///calculate midpoint between random vertex and the last point in the vector
+		double midPointx = midpoint(points.at(count-1).x, vertices.at(randomVertex).x);
+		double midPointy = midpoint(points.at(count-1).y, vertices.at(randomVertex).y);
             ///push back the newly generated coord.
+		
+		
+		
+		
+
+
+		
         }
 
         /*
