d56ebcda62e8faea94256c9130efe0e1be207fb2
HennyHD <166639866+HennyHD@users.noreply.github.com>
2024-04-16 22:13:02 -0700

    Update ChaosGamePenta.cpp
    more explanations

--- code/ChaosGamePenta.cpp
+++ code/ChaosGamePenta.cpp
@@ -29,87 +29,86 @@ int main()
 	text.setColor(Color::Red);
 	text.setString("OH HOW IT FEELS TO BE ALIVE");  //Set a string into the buffer
 	
-
-    vector<Vector2f> vertices;
+	//Declaring more variables
+    vector<Vector2f> vertices;  //Vectors to track and record clicks and positions
     vector<Vector2f> points;
-    double randomVertex = rand() % 5;
-    double randLast = randomVertex;
+    double randomVertex = rand() % 5;  //formula to pick random vertices notice size 5 for penta
+    double randLast = randomVertex;  //Tracking what the last Vertices is for unique penta
 
 	while (window.isOpen())
-	{
+		{
         	/*
 		****************************************
 		Handle the players input
 		****************************************
 		*/
         Event event;
-		while (window.pollEvent(event))
+		while (window.pollEvent(event))  //open a window to start the program
 		{
-            if (event.type == Event::Closed)
+            if (event.type == Event::Closed)  //check to close
             {
 				// Quit the game when the window is closed
 				window.close();
             }
-            if (event.type == sf::Event::MouseButtonPressed)
+            if (event.type == sf::Event::MouseButtonPressed)  //check mouse presses
             {
-                if (event.mouseButton.button == sf::Mouse::Left)
+                if (event.mouseButton.button == sf::Mouse::Left)  //left mouse actions, track 5 for vertices, 1 for starter point then automate
                 {
-                    std::cout << "the left button was pressed" << std::endl;
+                    std::cout << "the left button was pressed" << std::endl;  //record and display left mouse inputs on terminal
                     std::cout << "mouse x: " << event.mouseButton.x << std::endl;
                     std::cout << "mouse y: " << event.mouseButton.y << std::endl;
 
-                    if(vertices.size() < 5)
-                    {
-                        vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
+                    if(vertices.size() < 5)  
+                    {	//first 5 cliks
+                        vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));  //record in vector
                     }
                     else if(points.size() == 0)
                     {
-                        ///fourth click
-                        ///push back to points vector
+                        //6th click
                         points.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
                     }
                 }
             }
         }
-        if (Keyboard::isKeyPressed(Keyboard::Escape))
+        if (Keyboard::isKeyPressed(Keyboard::Escape)) //get me out button
 		{
 			window.close();
 		}
-        	/*
+
+		
+		/*
 		****************************************
 		Update
 		****************************************
 		*/
+
+		
 	double count = points.size() - 1;
         if(points.size() > 0)
         {
-            ///generate more point(s)
-            ///select random vertex
-            ///calculate midpoint between random vertex and the last point in the vector
-            ///push back the newly generated coord.
-            	//Triangle
-            	
+            //generate more point(s)
+            //select random vertex
             	double count = points.size() - 1;
             	while (randLast == randomVertex) {  // no duplicated vertex
-            		randomVertex = rand() % 5;
+            		randomVertex = rand() % 5;  
             	}
             	randLast = randomVertex;
-            	double midPointx = (points.at(count).x + vertices.at(randomVertex).x) * 0.5;
+            	double midPointx = (points.at(count).x + vertices.at(randomVertex).x) * 0.5;  //Calculate midpoint
 		double midPointy = (points.at(count).y + vertices.at(randomVertex).y) * 0.5;
-		points.push_back(Vector2f(midPointx, midPointy));
+		points.push_back(Vector2f(midPointx, midPointy));  //log new midpoint into vector
 	}
-		
-		
 
-
+		
         	/*
 		****************************************
 		Draw
 		****************************************
 		*/
+
+		
         window.clear();
-	window.draw(text);  // Display text buffer
-        for(int i = 0; i < vertices.size(); i++)
+	window.draw(text);  // Display text buffer from beginning
+        for(int i = 0; i < vertices.size(); i++)  //loop to draw rectancles of vertices
         {
             RectangleShape rect(Vector2f(5,5));
             rect.setPosition(Vector2f(vertices[i].x, vertices[i].y));
@@ -117,13 +116,13 @@ int main()
             window.draw(rect);
         }
      
-     	for(int i = 0; i < points.size(); i++){
+     	for(int i = 0; i < points.size(); i++){  //loop to draw rectancles of points
             RectangleShape rect(Vector2f(5,5));
             rect.setPosition(Vector2f(points[i].x, points[i].y));
             rect.setFillColor(Color::Yellow);
             window.draw(rect);
             }
         
-        window.display();
+        window.display(); //frame 1 iteration
     }
 }
